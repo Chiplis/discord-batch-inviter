4a34da3d08e24023441788213415557950857829
Nicolás Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2022-04-21 02:19:25 +0200

    Start using expiration argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::{max};
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::thread::sleep;
@@ -24,25 +25,26 @@ struct Args {
 
     /// Invite lifetime in seconds. Max: 604800 (one week).
     #[clap(short, long, default_value_t = 604800)]
-    expiration: u32,
+    expiration: u64,
 }
 
 fn main() {
-    let mut count = 0;
     let args = Args::parse();
     let token = &args.token.to_owned();
     let id = &args.id;
+    let expiration = max(args.expiration, args.count as u64 * 2) as u64;
     let ds = Discord::from_user_token(token).unwrap();
     let file_name = format!("invites_{:?}", SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()).to_owned();
     let mut file = OpenOptions::new().append(true).create(true).open(file_name).unwrap();
     println!();
+    let mut count = 0;
     for _ in 0..=args.count / 5  {
         for _ in 0..5 {
-            count += 1;
-            let code = ds.create_invite(ChannelId(*id), 604800 - count, 1, false).unwrap().code;
+            let code = ds.create_invite(ChannelId(*id), expiration - count, 1, false).unwrap().code;
             let invite = format!("discord.gg/{code}\n");
             println!("{invite}");
             file.write_all(invite.as_bytes()).unwrap();
+            count += 1;
         }
         sleep(Duration::from_secs(15));
     }
