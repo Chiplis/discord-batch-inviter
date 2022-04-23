8ee64cdc5dac93838845b5f3f2c31fe6f0820e76
Nicolás Siplis <nicolas.siplis.@gmail.com>
2022-04-23 14:07:03 +0200

    Run rustfmt, fix arguments value parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::cmp::{min};
+use clap::{CommandFactory, Parser};
+use discord::model::ChannelId;
+use discord::Discord;
+use std::cmp::min;
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Write};
 use std::thread::sleep;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
-use discord::Discord;
-use discord::model::{ChannelId};
-use clap::{Parser, CommandFactory};
 
 /// Simple Discord script to automatically generate mass invites
 #[derive(Parser, Debug)]
@@ -45,16 +45,19 @@ struct Args {
 
     /// Seconds to wait between round of operations.
     #[clap(short, long, default_value_t = 15)]
-    timeout: u8
+    timeout: u8,
 }
 
 struct RateLimit {
     timeout: u8,
-    round_size: u8
+    round_size: u8,
 }
 
 impl RateLimit {
-    fn execute<T, F>(&self, args: Vec<T>, mut op: F) where F: FnMut(T){
+    fn execute<T, F>(&self, args: Vec<T>, mut op: F)
+    where
+        F: FnMut(T),
+    {
         let mut rate_limiter = 0;
         for arg in args {
             rate_limiter += 1;
@@ -70,13 +73,27 @@ impl RateLimit {
 fn main() {
     let _ = Args::command().term_width(0).get_matches();
     let args = Args::parse();
-    let Args { bot, user, delete, id, amount, lifetime, max_uses, timeout, round_size } = args;
+    let Args {
+        bot,
+        user,
+        delete,
+        id,
+        amount,
+        lifetime,
+        max_uses,
+        timeout,
+        round_size,
+    } = args;
 
-    let rate_limit = RateLimit { timeout, round_size };
+    let rate_limit = RateLimit {
+        timeout,
+        round_size,
+    };
 
     let channel = ChannelId(id);
-    let count = min(amount, 100);
-
+    let max_uses = min(max_uses, 100);
+    let amount = min(amount, 1000);
+    let lifetime = min(lifetime, 604800);
 
     let ds = if let Some(u) = user {
         Discord::from_user_token(&u)
@@ -84,23 +101,34 @@ fn main() {
         Discord::from_bot_token(&b)
     } else {
         unreachable!()
-    }.expect("Error creating Discord client.");
+    }
+    .expect("Error creating Discord client.");
 
     println!();
 
     match delete {
         Some(d) => delete_invites(ds, d, channel, rate_limit),
-        None => create_invites(ds, count, id, lifetime, max_uses, rate_limit)
+        None => create_invites(ds, amount, id, lifetime, max_uses, rate_limit),
     }
 }
 
 fn delete_invites(ds: Discord, invites_file: String, channel: ChannelId, rate_limit: RateLimit) {
-    let invite_codes: Vec<String> = match invites_file.as_str() {
+    let invite_codes: Vec<String> = match invites_file.as_str().trim() {
         "" => {
             println!("No invite file specified, deleting all channel invites.");
-            ds.get_channel_invites(channel).expect("Error getting all channel invites to delete.").iter().map(|ri| ri.code.clone()).collect()
-        },
-        _ => BufReader::new(File::open(invites_file).expect("Error opening file with invites to delete.")).lines().into_iter().map(|l| l.unwrap()).collect()
+            ds.get_channel_invites(channel)
+                .expect("Error getting all channel invites to delete.")
+                .iter()
+                .map(|ri| ri.code.clone())
+                .collect()
+        }
+        _ => BufReader::new(
+            File::open(invites_file).expect("Error opening file with invites to delete."),
+        )
+        .lines()
+        .into_iter()
+        .map(|l| l.unwrap())
+        .collect(),
     };
     rate_limit.execute(invite_codes, |code| {
         if let Err(e) = ds.delete_invite(&code) {
@@ -111,14 +139,38 @@ fn delete_invites(ds: Discord, invites_file: String, channel: ChannelId, rate_li
     })
 }
 
-fn create_invites(ds: Discord, max_invites: u16, id: u64, max_age: u32, max_uses: u8, rate_limit: RateLimit) {
-    let file_name = format!("invites_{:?}", SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()).to_owned();
-    let mut file = OpenOptions::new().append(true).create(true).open(file_name).unwrap();
-    rate_limit.execute((1..=max_invites).into_iter().collect(), move |invite_count| {
-        println!("{invite_count} / {max_invites}");
-        let code = ds.create_invite(ChannelId(id), max_age, max_uses, false, true).unwrap().code;
-        let invite = format!("discord.gg/{code}\n");
-        println!("{invite}");
-        file.write_all(invite.as_bytes()).unwrap();
-    });
+fn create_invites(
+    ds: Discord,
+    amount: u16,
+    id: u64,
+    max_age: u32,
+    max_uses: u8,
+    rate_limit: RateLimit,
+) {
+    let file_name = format!(
+        "invites_{:?}",
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
+    )
+    .to_owned();
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(file_name)
+        .unwrap();
+    rate_limit.execute(
+        (1..=amount).into_iter().collect(),
+        move |invite_count| {
+            println!("{invite_count} / {amount}");
+            let code = ds
+                .create_invite(ChannelId(id), max_age, max_uses, false, true)
+                .unwrap()
+                .code;
+            let invite = format!("discord.gg/{code}\n");
+            println!("{invite}");
+            file.write_all(invite.as_bytes()).unwrap();
+        },
+    );
 }
